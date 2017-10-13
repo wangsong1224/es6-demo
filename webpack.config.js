ea7cfc59ce5b037bf8b00c4e84d06d43e1c14c6a
wnagsong <wangsong1@bonc.com.cn>
2017-10-13 10:50:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry:'./index.html',
+  output:{
+    path:path.resolve(__dirname,'dist')
+  },
+  filename:'bundle.js',
+  module:{
+    rules:[
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins:[
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new FriendlyErrorsPlugin()
+  ]
+}
